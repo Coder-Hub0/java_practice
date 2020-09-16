8d07a063d2be748e661f29ebf1bb7ba9dda7eda0
DarkDevil <deepanshugupta102@gmail.com>
2020-09-16 16:15:00 +0530

    7th Example

---
+++ src/org/deep/core/JavaNestedIfExample.java
@@ -0,0 +1,13 @@
+package org.deep.core;
+
+public class JavaNestedIfExample {
+public static void main(String ...strings)
+{
+	int age=20;
+	int weight=80;
+	if(age>=18) {
+		if(weight>=50) {
+			System.out.println(" you are eligible to donate blood");
+		}
+	}}
+}
