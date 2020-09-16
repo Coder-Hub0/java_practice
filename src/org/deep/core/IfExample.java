8c88e7c22f7210e49637d45f32eb5e642695eb5f
DarkDevil <deepanshugupta102@gmail.com>
2020-09-16 15:14:11 +0530

    2nd Program

---
+++ src/org/deep/core/IfExample.java
@@ -0,0 +1,16 @@
+package org.deep.core;
+
+public class IfExample {
+
+	public static void main(String ...args)
+	{
+		//defining an 'age' variable
+		int age=20;
+		//checking the age
+		if(age>18)
+		{
+			System.out.println("age is greater than 18");
+		}
+	}
+	
+}
