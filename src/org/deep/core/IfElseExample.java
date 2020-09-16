766e76df072c166932516b1413d4316b3c868c4f
DarkDevil <deepanshugupta102@gmail.com>
2020-09-16 15:14:36 +0530

    3rd Program

---
+++ src/org/deep/core/IfElseExample.java
@@ -0,0 +1,16 @@
+package org.deep.core;
+
+public class IfElseExample {
+
+	public static void main(String... strings) {
+		// defining a variable
+		int number = 13;
+		// check if the number is divisible by 2 or not
+		if (number % 2 == 0) {
+			System.out.println("even Number");
+		} else {
+			System.out.println("odd Number");
+		}
+
+	}
+}
