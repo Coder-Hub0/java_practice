e91af63eeae5648e7792d61719f5e01348654c5d
DarkDevil <deepanshugupta102@gmail.com>
2020-09-16 15:15:13 +0530

    4th Example

---
+++ src/org/deep/core/IfElseTernaryExample.java
@@ -0,0 +1,13 @@
+package org.deep.core;
+
+public class IfElseTernaryExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number=13;
+		//using ternary operator
+		String output=(number%2==0)?"Even number":"Odd Number";
+		System.out.println(output);
+	}
+
+}
