4e886475247ac99b68d444f486926959bad9f523
demonvilla <demonvilla@i>
2020-09-16 12:40:47 +0530

    First Program

---
+++ src/org/deep/core/HelloWorld.java
@@ -0,0 +1,12 @@
+package org.deep.core;
+/*
+ * First Program
+ * in which we able to see how to print message "Hello World" on screen
+ * 
+ */
+public class HelloWorld {
+	public static void main(String... strings) {
+	
+		System.out.println("Hello World");
+	}
+}
