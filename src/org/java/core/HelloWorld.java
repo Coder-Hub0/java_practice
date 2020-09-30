76d7b21cf32c5f8730eecb6d03d84f51507490ae
DarkDevil <deepanshugupta102@gmail.com>
2020-09-30 13:49:11 +0530

    Printing user Screen

---
+++ src/org/java/core/HelloWorld.java
@@ -0,0 +1,14 @@
+package org.java.core;
+
+/*
+ * Write a program to print welcome message on the Screen.
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java World");
+		
+
+	}
+
+}
